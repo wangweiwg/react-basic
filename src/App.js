288ae3a57ebc50ae29cf0d57728ff4bd7b3c97bf
wangweiwg <wangwei641@163.com>
2022-07-18 09:44:30 +0800

    app 清除

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-function App() {
+import React, { createContext } from 'react';
+const { Provider, Consumer } = createContext()
+
+function ComA () {
   return (
-    <div className="App">app</div>
-  );
+    <div>
+      <p>this is ComponentA</p>
+      <ComC />
+    </div>
+  )
+}
+
+function ComC () {
+  return (
+    <div>
+      <p>this is ComponentA</p>
+      <Consumer>{ value => value}</Consumer>
+    </div>
+  )
+}
+class App extends React.Component {
+  state = {
+    msg: 'this is msg'
+  }
+  render() {
+    return (
+      <Provider value={this.state.msg}>
+        <div>
+          <p>App</p>
+          <ComA />
+        </div>
+      </Provider>
+    )
+  }
 }
 
 export default App;
