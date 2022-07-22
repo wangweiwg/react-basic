363d78ad458358c307d4743141dbbc1b10ed7379
wangweiwg <wangwei641@163.com>
2022-07-22 11:05:59 +0800

    test: prettier配置

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react'
 
 function App() {
+  const test = () => {
+    console.log('test')
+  }
+  test()
   return (
-    <div style={{ height: "2000px" }}>
+    <div style={{ height: '2000px' }}>
       <p>App---{process.env.NODE_ENV}</p>
       <p>自定义环境：{String(process.env.REACT_APP_NOT_SECRET_CODE)}</p>
+      <p>自定义环境：{String(process.env.REACT_APP_NOT_SECRET_CODE)}</p>
+      <p>自定义环境：{String(process.env.REACT_APP_NOT_SECRET_CODE)}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
